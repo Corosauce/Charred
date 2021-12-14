70570c3abeb3950b606fbf49df1403b0bd235b6c
Corosauce <cordonfreeman@gmail.com>
2021-12-14 17:42:32 +0000

    Initial everything, working mod but very broken blockstate stuff, extra block states not working, blockshapes hacked to return solid block, leaving for future self to figure out.

---
+++ src/main/java/com/corosus/CharredRegistry.java
@@ -0,0 +1,24 @@
+package com.corosus;
+
+import com.corosus.block.CharredLogBlock;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class CharredRegistry {
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Charred.MODID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Charred.MODID);
+
+    public static final RegistryObject<Block> BLOCK_CHARRED_LOG = BLOCKS.register("charred_log", () -> new CharredLogBlock(BlockBehaviour.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(3.0F, 3.0F)));
+    public static final RegistryObject<Item> ITEM_CHARRED_LOG = ITEMS.register("charred_log", () -> new BlockItem(CharredRegistry.BLOCK_CHARRED_LOG.get(), new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+}
