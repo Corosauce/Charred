6c0eb6928bcc990a4ec580b0a4e59d6a8064a0ee
SH42913 <shoot913@gmail.com>
2022-04-03 13:46:04 +0300

    Up to Forge 40.0.36

--- src/main/java/com/corosus/mixin/FireToCharcoal.java
+++ src/main/java/com/corosus/mixin/FireToCharcoal.java
@@ -19,7 +19,7 @@ public abstract class FireToCharcoal {
     public boolean removeBlock(Level instance, BlockPos p_46623_, boolean p_46624_) {
         //hooking code that removes the block on fire
         BlockState state = instance.getBlockState(p_46623_);
-        if (BlockTags.LOGS.contains(state.getBlock())) {
+        if (state.is(BlockTags.LOGS)) {
             return instance.setBlock(p_46623_, CharredRegistry.BLOCK_CHARRED_LOG.get().defaultBlockState(), 3 | (p_46624_ ? 64 : 0));
         } else {
             return instance.removeBlock(p_46623_, p_46624_);
@@ -32,7 +32,7 @@ public abstract class FireToCharcoal {
     public boolean setBlock(Level instance, BlockPos p_46601_, BlockState p_46602_, int p_46603_) {
         //hooking code that tries to place more fire in place of the block on fire
         BlockState state = instance.getBlockState(p_46601_);
-        if (BlockTags.LOGS.contains(state.getBlock())) {
+        if (state.is(BlockTags.LOGS)) {
             return instance.setBlock(p_46601_, CharredRegistry.BLOCK_CHARRED_LOG.get().defaultBlockState(), p_46603_);
         } else {
             return instance.removeBlock(p_46601_, false);
